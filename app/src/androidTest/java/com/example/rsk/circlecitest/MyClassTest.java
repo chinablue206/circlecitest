780b3b9e5dc63e5b08615706fbd0469255a5f26b
ryousuke <rsksato@gmail.com>
2015-10-17 15:58:32 +0900

    initial project files

---
+++ app/src/androidTest/java/com/example/rsk/circlecitest/MyClassTest.java
@@ -0,0 +1,10 @@
+package com.example.rsk.circlecitest;
+
+import junit.framework.TestCase;
+
+public class MyClassTest extends TestCase {
+
+    public void testHoge() throws Exception {
+
+    }
+}
---
+++ app/src/main/java/com/example/rsk/circlecitest/MyClass.java
@@ -0,0 +1,10 @@
+package com.example.rsk.circlecitest;
+
+public class MyClass {
+    private static final String TAG = MyClass.class.getSimpleName();
+    private final MyClass self = this;
+
+    static public String hoge () {
+        return "HOGE";
+    }
+}
